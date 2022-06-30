bcdf7f131e46e14ffaf4056e4d045df4fe9d52a2
Shuvo <sohorafuzzamanshuvo@gmail.com>
2022-06-30 21:56:43 +0600

    Forgot password change activity test

--- app/src/main/java/com/example/myloginapp/MainActivity.java
+++ app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -2,10 +2,19 @@ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -16,9 +25,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username=findViewById(R.id.username);
-        TextView pass=findViewById(R.id.pass);
+        EditText username=findViewById(R.id.username);
+        EditText pass=findViewById(R.id.pass);
+        TextView forgotPass=findViewById(R.id.forgotpass);
         MaterialButton loginButton=(MaterialButton) findViewById(R.id.login);
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,11 +40,25 @@ public class MainActivity extends AppCompatActivity {
 
                 }else{
                     Toast.makeText(getApplicationContext(), "Invalid username or password", Toast.LENGTH_SHORT).show();
-
                 }
+            }
+        });
+
+//        public void onLoginClick(new View){
+//            startActivity(new Intent(this,test.class));
+//            //overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+//
+//        }
+
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),test.class));
 
             }
         });
+
+
 
     }
 }
